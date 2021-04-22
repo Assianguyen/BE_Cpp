894bcd302f1d4e3b973850edee24e26e2f72568a
Assianguyen <assianguyen@yahoo.fr>
2021-04-22 18:05:53 +0200

    test merge

--- Projet_health_monitoring/Actuator.cpp
+++ Projet_health_monitoring/Actuator.cpp
@@ -1,1 +1,1 @@
-
+#include "Actuator.h"
--- Projet_health_monitoring/Projet_health_monitoring.ino
+++ Projet_health_monitoring/Projet_health_monitoring.ino
@@ -12,6 +12,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  Serial.println("salut");
   monitor.startMonitoring();
 }
