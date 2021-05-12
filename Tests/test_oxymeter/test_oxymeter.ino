1e02aade1cc109cc35508cc51fb4c73c4dd35bd7
Assianguyen <assianguyen@yahoo.fr>
2021-05-12 14:45:30 +0200

    Test de l'oxymètre -inutile

--- Tests/test_oxymeter/test_oxymeter.ino
+++ Tests/test_oxymeter/test_oxymeter.ino
@@ -1,15 +1,16 @@
-#define heartratePin A1
+#define heartratePin A0
 #include"DFRobot_Heartrate.h"
 
-DFRobot_Heartrate heartrate(DIGITAL_MODE);
+DFRobot_Heartrate heartrate(ANALOG_MODE);
 
 ///< ANALOG_MODE or DIGITAL_MODE
-voidsetup(){  
+void setup(){  
   Serial.begin(115200);
+  Serial.println("setup done");
   }
-  voidloop(){
+  void loop(){
     uint8_t rateValue;
-    heartrate.getValue(heartratePin);///< A1 foot sampled values  
+    heartrate.getValue(heartratePin);///< A0 foot sampled values  
     rateValue = heartrate.getRate();///< Get heart rate value 
     if(rateValue){    
       Serial.println(rateValue);
